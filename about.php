72ec665b624a4df881d218f9fd195214fb97f9dc
Dimas1342 <117787063+Dimas1342@users.noreply.github.com>
2022-11-09 12:23:43 +0700

    Initial commit

---
+++ about.php
@@ -0,0 +1,40 @@
+<?php 
+	include 'layouts/sidebar.php';
+	include 'layouts/topbar.php';
+	include 'koneksi.php';
+ ?>
+<style type="text/css">
+	img{
+		border-radius: 10px;
+	}
+</style>
+<div class="container-fluid">
+	<h2>About</h2>
+<h4>Aplikasi ini dibuat Oleh :</h4>
+<p ><img src="images/Dimas.jpg" width="150" height="200" style="float:left;" />
+	
+	<table>
+		<tr >
+			<td height="30px"><h5>Nama</h5></td>
+			<td> <h5>:</h5> </td>
+			<td><h5>Dimas Tri Atmaja</h5></td>
+		</tr>
+		<tr>
+			<td height="30px"><h5>Nim</h5></td>
+			<td> <h5>:</h5> </td>
+			<td><h5>2031730088</h5></td>
+		</tr>
+		<tr>
+			<td height="30px"><h5>Tanggal</h5></td>
+			<td> <h5>:</h5> </td>
+			<td><h5>8 November 2022</h5></td>
+		</tr>
+		
+	</table>
+</p>	
+
+</div>
+</div>
+ <?php 
+ 	include 'layouts/footer.php';
+  ?>
